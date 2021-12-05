bb035a13406adcfc3a31e97cf3f5927f26ae4937
SarahAmmar <saraammar170@gmail.com>
2021-12-05 13:35:04 +0200

    first

---
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -0,0 +1,25 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class HttpHeadersInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        'x-rapidapi-key': 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
+        'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+      },
+      setParams: {
+        //I got an error when I didn't use my own key 
+        key: '99a6d544f1264219badbd72bc895691f',
+      }
+    });
+    return next.handle(req);
+  }
+}
